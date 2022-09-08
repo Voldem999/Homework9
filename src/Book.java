455c0874ed0ce1563441033062ddf2f71df80ff8
Ilya201091 <ZiK201091@ya.ru>
2022-09-09 00:14:06 +0700

    выполнил ДЗ

---
+++ src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    private String name;
+    private int publishYear;
+    private Author author;
+
+    public Book(String name, int publishYear, Author author) {
+        this.name = name;
+        this.publishYear = publishYear;
+        this.author = author;
+    }
+}
